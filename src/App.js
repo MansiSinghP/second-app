64827b4b177f1fefdb59cbbb27fba73c5ebcc175
Mansi <mansiparihar1999@gmail.com>
2023-10-11 11:23:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [darkmode,setDarkmode]=useState(false);
   return (
     <>
 <Navbar title="TextUtils" aboutText="About TextUtils"/>
 <div className="container my-3">           
+<About/>
 <TextForm heading ="Enter the text to analyse"/>
 </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,6 +16,18 @@ export default function TextForm(props) {
         console.log("on change!")
         setText(event.target.value);      //makes textarea typable.
     }
+    const handleClearClick=()=>{
+        console.log("clear!")
+        let newTxt=' ';
+        setText(newTxt);
+    }
+    const handleCopy=()=>{
+        console.log("I am copy");
+        var text=document.getElementById('mybox');
+        text.select();
+        
+        navigator.clipboard.writeText(text.value);
+    }
 
     const [text, setText] = useState('Enter Text here');
    // setText("New Text"); //correct way to change state
@@ -28,7 +40,8 @@ export default function TextForm(props) {
 </div>
 <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert into uppercase</button>
 <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert To LowerCase</button>
-    
+<button className='btn btn-primary mx-2' onClick={handleClearClick}>Clear</button> 
+<button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
     <div className="container my-2" >
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
